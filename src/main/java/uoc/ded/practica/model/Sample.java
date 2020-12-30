c13945878943639fb6aae976fbb66df6ed2bbc9f
jbericat <jbericat@uoc.edu>
2020-12-30 15:47:59 +0100

    v1.2

--- src/main/java/uoc/ded/practica/model/Sample.java
+++ src/main/java/uoc/ded/practica/model/Sample.java
@@ -1,5 +1,7 @@
 package uoc.ded.practica.model;
 
+import java.util.Date;
+
 import uoc.ded.practica.Trial4C19;
 import uoc.ded.practica.Trial4C19.Status;
 
@@ -7,46 +9,75 @@ public class Sample {
 	
 	private String idSample;
 	private Status status;
-	private String user;
+	private User user;
 	private String report;
+	private Date creationDate;
+	private Date shippingDate;
+	private Date completionDate;
 	
-	
-	public Sample() {
-		//TO-DO
+	public Sample(String idSample, Status status, User user, String report) {
+		this.setIdSample(idSample);
+		this.setStatus(status);
+		this.setUser(user);
+		this.setReport(report);
+		this.setDateCreation(now);
+		this.setDateSended(null);
+		this.setDateCompleted(null);
 	}
 
-	public Trial4C19.Status getStatus() {
-		// TODO Auto-generated method stub
-		return null;
+	private void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Status getStatus() {
+		return this.status;
+	}
+
+	private void setIdSample(String idSample) {
+		this.idSample = idSample;
 	}
 
 	public String getIdSample() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.idSample;
+	}
+	
+	private void setUser(User user) {
+		this.user = user;
 	}
 
 	public User getUser() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.user;
+	}
+	
+	private void setReport(String report) {
+		this.report = report;
 	}
 
 	public String getReport() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.report;
+	}
+	
+	private void setDateCreation(Date date) {
+		this.creationDate = date;
 	}
 
 	public Object getDateCreation() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.creationDate;
 	}
-
-	public Object getDateSended() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private void setDateSended(Date date) {
+		this.shippingDate = date;
+	}
+	
+	public Date getDateSended() {
+		return this.shippingDate;
+	}
+	
+	private void setDateCompleted(Date date) {
+		this.completionDate = date;
 	}
 
 	public Object getDateCompleted() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.completionDate;
 	}
 }
